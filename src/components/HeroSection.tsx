0aae8af152320f9a63ba2362ecbd2359259ae6a3
Abhishek Kumar <abhishekkumar958830@gmail.com>
2024-06-16 00:47:14 +0530

    minor bug fixing

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -20,8 +20,8 @@ function HeroSection() {
       > Master the art of music</h1>
       <p
       className='mt-4 font-normal text-base md:text-lg
-      text-neutral-300 max-w-lg mx-auto'
-      >Dive into our comprehensive music courses and transform
+      text-neutral-300 max-w-lg mx-auto'>
+        Dive into our comprehensive music courses and transform
         your musical journey today. Whether you're a beginner or looking
         to refine your skills, join us to unlock your true potential.
       </p>
